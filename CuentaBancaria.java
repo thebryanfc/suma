6354d4302aba542863482204f19651dcae83d40a
thebryanfc <124324101+thebryanfc@users.noreply.github.com>
2023-03-06 17:50:46 -0600

    Add files via upload

---
+++ CuentaBancaria.java
@@ -0,0 +1,47 @@
+public class CuentaBancaria {
+     String Name;
+     double Balance;
+     double NoCuenta;
+     String NombreBanco;
+     String Email;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public double getBalance() {
+        return Balance;
+    }
+
+    public void setBalance(double balance) {
+        Balance = balance;
+    }
+
+    public double getNoCuenta() {
+        return NoCuenta;
+    }
+
+    public void setNoCuenta(double noCuenta) {
+        NoCuenta = noCuenta;
+    }
+
+    public String getNombreBanco() {
+        return NombreBanco;
+    }
+
+    public void setNombreBanco(String nombreBanco) {
+        NombreBanco = nombreBanco;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+}
